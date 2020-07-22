8082621456e87ff09ee72a69d3d9d1e67bec7a51
Marcos Vidolin <marcosvidolin@gmail.com>
2020-07-22 00:17:17 -0300

    chore: htlm to json function

--- src/rastreio/index.ts
+++ src/rastreio/index.ts
@@ -5,6 +5,14 @@ interface Rastreio {
   status: string,
 };
 
+interface Evento {
+  status: string,
+  data: string,
+  local: string,
+  origem: string,
+  destino: string,
+};
+
 const rastrear = async (codigo: string): Promise<Rastreio | any> => {
   return axios({
     method: 'GET',
@@ -14,7 +22,38 @@ const rastrear = async (codigo: string): Promise<Rastreio | any> => {
       "cache-control": "no-cache",
     },
   }).then(resp => {
-    console.log(resp.data);
+    console.log(convertHtmlToJson(resp.data));
   });
+};
+
+
+function convertHtmlToJson(htmlString: string) {
+  const html = cheerio.load(htmlString);
+  const elemArray: CheerioElement[] = [];
+  html("ul.linha_status").each((_, elem) => {
+    elemArray.push(elem);
+  });
+  const elemMap = elemArray.map((elem) => {
+    const mapObj: any = {};
+    html(elem)
+      .find("li")
+      .each((_, liElem) => {
+        const text = html(liElem).text();
+        if (text) {
+          if (text.includes("Status")) mapObj.status = text;
+          if (text.includes("Data")) mapObj.data = text;
+          if (text.includes("Local")) mapObj.local = text;
+          if (text.includes("Origem")) mapObj.origem = text;
+          if (text.includes("Destino")) mapObj.destino = text;
+        }
+      });
+    return mapObj;
+  });
+
+  return elemMap.reverse();
+}
+
+
+
 
 export default rastrear;
