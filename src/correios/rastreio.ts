5c2e606b15bbdde07fc550ef187ad04ac3624722
Marcos Vidolin <marcosvidolin@gmail.com>
2021-01-10 02:26:12 -0300

    chore: replace cheerio with fast-html-parser

--- src/correios/rastreio.ts
+++ src/correios/rastreio.ts
@@ -1,5 +1,6 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
+import HTMLParser from 'fast-html-parser';
 
 export interface Rastreio {
   eventos?: Evento[],
@@ -29,43 +30,46 @@ export const rastrear = async (codigo: string): Promise<Rastreio> => {
   });
 };
 
+export const convertHtmlToEvento = (html: string): Evento[] => {
+  const root = HTMLParser.parse(html);
 
-export const convertHtmlToEvento = (htmlString: string): Evento[] => {
-  const html = cheerio.load(htmlString);
-  const elemArray: CheerioElement[] = [];
-  html("ul.linha_status").each((_, elem) => {
-    elemArray.push(elem);
+  const status = root.querySelectorAll('ul.linha_status');
+
+  const eventos: Evento[] = [];
+  status.forEach((elem) => {
+    const evento = convertElementToEvent(elem);
+    eventos.push(evento);
   });
-  const elemMap = elemArray.map((elem) => {
-    const mapObj: Evento = {};
-    html(elem)
-      .find("li")
-      .each((_, liElem) => {
-        const text = html(liElem).text();
-        if (text) {
-          mapObj.status = ''; // TODO: pendente, entregue, falha
-          if (text.includes("Status")) mapObj.nome = text.replace('Status:', '').trim();
-          if (text.includes("Data")) { 
-            const regex = /([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}/gm;
 
-            const strDate = regex.exec(text)?.[0];
-            if (strDate) {
-              const dateParts = strDate.split("/");
-              // month is 0-based, that's why we need dataParts[1] - 1
-              mapObj.data = new Date(+dateParts[2], Number(dateParts[1]) - 1, +dateParts[0]);
-            }
-          }
-          if (text.includes("Data")) { 
-            const regex = /([0-9][0-9])(:)([0-9][0-9])/gm;
-            mapObj.hora = regex.exec(text)?.[0];
-          }
-          if (text.includes("Local")) mapObj.local = text.replace('Local:', '').trim();
-          if (text.includes("Origem")) mapObj.origem = text.replace('Origem:', '').trim();
-          if (text.includes("Destino")) mapObj.destino = text.replace('Destino:', '').trim();
+  return eventos.reverse();
+}
+
+export const convertElementToEvent = (elem: HTMLParser.HTMLElement): Evento => {
+  const evento: Evento = {};
+  elem.childNodes.forEach((li) => {
+    const text = li.rawText;
+    if (text) {
+      evento.status = ''; // TODO: pendente, entregue, falha
+      if (text.includes("Status")) evento.nome = text.replace('Status:', '').trim();
+      if (text.includes("Data")) { 
+        const regex = /([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}/gm;
+
+        const strDate = regex.exec(text)?.[0];
+        if (strDate) {
+          const dateParts = strDate.split("/");
+          // month is 0-based, that's why we need dataParts[1] - 1
+          evento.data = new Date(+dateParts[2], Number(dateParts[1]) - 1, +dateParts[0]);
         }
-      });
-    return mapObj;
+      }
+      if (text.includes("Data")) { 
+        const regex = /([0-9][0-9])(:)([0-9][0-9])/gm;
+        evento.hora = regex.exec(text)?.[0];
+      }
+      if (text.includes("Local")) evento.local = text.replace('Local:', '').trim();
+      if (text.includes("Origem")) evento.origem = text.replace('Origem:', '').trim();
+      if (text.includes("Destino")) evento.destino = text.replace('Destino:', '').trim();
+    }
   });
 
-  return elemMap.reverse();
-};
+  return evento;
+}
--- tests/rastreio.test.ts
+++ tests/rastreio.test.ts
@@ -10,10 +10,10 @@ test('should convert the HTML response from Correios into a list of Events', asy
 
   const eventos: Evento[] = convertHtmlToEvento(htmlResponseFromCorreiosMock);
 
-  expect(eventos.length).toBe(3);
-  expect(eventos[2].nome).toEqual('Objeto em trânsito - por favor aguarde');
-  expect(eventos[2].data).toEqual(new Date(2020, 12-1, 29));
-  expect(eventos[2].hora).toEqual('11:10');
-  expect(eventos[2].origem).toEqual('HONG KONG - /');
-  expect(eventos[2].destino).toEqual('Unidade de Tratamento Internacional - / BR');
+  expect(eventos.length).toBe(6);
+  expect(eventos[1].nome).toEqual('Objeto em trânsito - por favor aguarde');
+  expect(eventos[1].data).toEqual(new Date(2020, 12-1, 29));
+  expect(eventos[1].hora).toEqual('11:10');
+  expect(eventos[1].origem).toEqual('HONG KONG - /');
+  expect(eventos[1].destino).toEqual('Unidade de Tratamento Internacional - / BR');
 })
