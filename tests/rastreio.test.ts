907683c339b0421c154a51d8394e5c1f9e417655
Marcos Vidolin <marcosvidolin@gmail.com>
2021-02-13 00:07:46 -0300

    test: call service

--- tests/rastreio.test.ts
+++ tests/rastreio.test.ts
@@ -4,16 +4,18 @@ import {getHtmlResponse} from './mock'
 import {Evento} from '../src/correios/rastreio'
 
 test('should convert the HTML response from Correios into a list of Events', async () => {
-  // const resp = await rastrear('LB089250735HK');
+  
+  const rastreio = await rastrear('LB089250735HK');
+  const eventos = rastreio?.eventos;
 
-  const htmlResponseFromCorreiosMock = getHtmlResponse();
+  console.log(eventos)
 
-  const eventos: Evento[] = convertHtmlToEvento(htmlResponseFromCorreiosMock);
+  // const eventos: Evento[] = convertHtmlToEvento(htmlResponseFromCorreiosMock);
 
-  expect(eventos.length).toBe(6);
-  expect(eventos[1].nome).toEqual('Objeto em trânsito - por favor aguarde');
-  expect(eventos[1].data).toEqual(new Date(2020, 12-1, 29));
-  expect(eventos[1].hora).toEqual('11:10');
-  expect(eventos[1].origem).toEqual('HONG KONG - /');
-  expect(eventos[1].destino).toEqual('Unidade de Tratamento Internacional - / BR');
+  // expect(eventos.length).toBe(6);
+  // expect(eventos[1].nome).toEqual('Objeto em trânsito - por favor aguarde');
+  // expect(eventos[1].data).toEqual(new Date(2020, 12-1, 29));
+  // expect(eventos[1].hora).toEqual('11:10');
+  // expect(eventos[1].origem).toEqual('HONG KONG - /');
+  // expect(eventos[1].destino).toEqual('Unidade de Tratamento Internacional - / BR');
 })
